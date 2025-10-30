1543b75f75e90a4d9dcae0cd69dd4995a22debb8
unknown <lindiemk34@gmail.com>
2025-10-30 14:20:48 +0200

    Rebuild after code edits

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1996005891"
+    serviceWorkerVersion: "91621225"
   }
 });
